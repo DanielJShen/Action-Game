b0667f2ce8eb2622cdedc920c62381d4460ecf11
Daniel <6959648+DanielJShen@users.noreply.github.com>
2018-02-23 16:07:11 +0000

    Added inventory class

---
+++ Classes/Inventory.py
@@ -0,0 +1,15 @@
+from Classes.Abilities.Ability import Ability
+
+class Inventory:
+
+    def __init__(self):
+        self.enabledAbility = Ability
+        self.abilities = []
+        self.activePowerups = []
+        self.powerups = []
+
+    def enableAbility(self,ability):
+        pass
+
+    def usePowerUp(self,powerup):
+        pass
