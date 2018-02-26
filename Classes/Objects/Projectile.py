1afd036dfbe533d38fa0bcc26a2b869f78dfdb35
Daniel <djs.shen@gmail.com>
2018-02-26 18:56:59 +0000

    Added basic collision with a line based on lecture

---
+++ Classes/Objects/Projectile.py
@@ -0,0 +1,23 @@
+from Classes.Vector import Vector
+class Projectile:
+    def __init__(self,vel,pos,radius,timer,bounceState,damage): #rof is Rate of Fire
+        self.vel:Vector = vel
+        self.pos = pos
+        self.radius = radius
+        self.timer = timer
+        self.bounceState = bounceState #Whether an object bounces of the wall
+        self.damage = damage
+
+    def draw(self,canvas):
+        canvas.draw_circle(self.pos.getP(),self.radius,0.001,'green','green')
+    def update(self):
+        self.pos.add(self.vel)
+
+    def bounce(self): #Checks if the projectile hits a wall
+        return True
+
+    def timer(self): #Used to end a projectile life
+        pass
+
+
+#The projectile class is the object that is shot by the player e.g a bullet OR LASERS WHOOOOOOOOO
--- Main.py
+++ Main.py
@@ -2,15 +2,39 @@ try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+from Classes.Objects.Projectile import Projectile
+from Classes.Interactions import Interactions
+from Classes.Objects.Wall import Wall
+from Classes.Vector import Vector
+
+CANVAS_HEIGHT=1000
+CANVAS_WIDTH=1600
 
 def click():pass
 
+projectile = Projectile(Vector(10,8),Vector(50,50),20,-1,True,0)
+wall = Wall(Vector(0,0),Vector(200,500),Vector(500,300))
+interaction = Interactions()
+
 # Handler to draw on canvas
 def draw(canvas):
-    canvas.draw_text("message", [50,112], 48, "Red")
+    canvas.draw_text("Testing", [50,112], 48, "Red")
+    projectile.draw(canvas)
+    projectile.update()
+    wall.draw(canvas)
+
+
+    projectiles = [projectile]
+    walls = [wall]
+    interaction.bounceBallOffWall(projectiles,walls)
+
+    for proj in projectiles:
+        proj.pos.x %= CANVAS_WIDTH
+        proj.pos.y %= CANVAS_HEIGHT
+
 
 # Create a frame and assign callbacks to event handlers
-frame = simplegui.create_frame("Home", 300, 200)
+frame = simplegui.create_frame("Home", CANVAS_WIDTH, CANVAS_HEIGHT)
 frame.add_button("Click me", click)
 frame.set_draw_handler(draw)
 
