138d08c05fc4d66f91bc6fcffa147cd591f7ca5e
Daniel <djs.shen@gmail.com>
2018-02-27 16:00:51 +0000

    Added Wall and collisions with projectiles

--- Classes/Objects/Wall.py
+++ Classes/Objects/Wall.py
@@ -1,13 +1,18 @@
 from Classes.Vector import Vector
 import math
 class Wall:
-    def __init__(self,vel,pos1,pos2):
+    def __init__(self,vel,thickness,pos1,pos2):
         self.vel = vel
+        self.thickness = thickness/2
         self.pos1 : Vector = pos1
         self.pos2 : Vector = pos2
 
+        self.line = self.pos2.copy().subtract(self.pos1)
+
     def draw(self,canvas):
-        canvas.draw_line(self.pos1.getP(), self.pos2.getP(), 2, "white")
+        canvas.draw_polyline([self.pos1.getIntP(), self.pos2.getIntP()], int(self.thickness*2), "white")
+        # canvas.draw_circle(self.pos1.getP(), self.thickness-1, 1, "white", "white")
+        # canvas.draw_circle(self.pos2.getP(), self.thickness-1, 1, "white", "white")
     def update(self):
         self.pos.add(self.vel)
 
@@ -17,32 +22,46 @@ class Wall:
     def timer(self): #Used to end a projectile life
         pass
 
-    def reflect(self,projectile):
-        #Trigonometry to find the distance from the point to the line
+    def distanceTo(self,projectile): #Trigonometry to find the distance from the point to the line
         line1 = self.pos1.copy().subtract(projectile.pos)
-        line2 = self.pos2.copy().subtract(self.pos1)
-        angle = line1.angle(line2)
-        seperation = line1.length()*math.sin(angle)
-        #Trig to see if the projectile is past the end of the line
-        projectileProjected = projectile.pos.getProj(line2.getNormalized()).length() #projectile.pos.length()*math.cos(projectile.pos.copy().negate().angle(line2))
-        point1Projected = self.pos1.getProj(line2.getNormalized()).length() #self.pos1.length()*math.cos(self.pos1.copy().negate().angle(line2))
-        point2Projected = self.pos2.getProj(line2.getNormalized()).length() #self.pos2.length()*math.cos(self.pos2.angle(line2))
-        option1 = projectileProjected >= point1Projected and projectileProjected <= point2Projected
-        option2 = projectileProjected >= point2Projected and projectileProjected <=point1Projected
-        if projectile.radius >= seperation and (option1 or option2):
-            print('#################')
-            #Calculate how to free Object
-            direction:Vector = projectile.vel.copy().negate().normalize()
-            distance = (projectile.radius-seperation)/math.sin(line2.angle(direction))
+        angle = line1.angle(self.line)
+        return line1.length()*math.sin(angle)
+    def inBounds(self,projectile): #Checks if projectile is past the end of the line
+        return ((projectile.pos - self.pos1).dot(self.line.getNormalized()) >= 0 and
+                (projectile.pos - self.pos2).dot(-self.line.getNormalized()) >= 0)
+    def reflectEdge(self,projectile): #Checks if the projectile is colliding with the end of the line
+        #End 1
+        seperation = self.pos1.copy().subtract(projectile.pos)
+        if (self.thickness) + projectile.radius >= seperation.length():
+            incSelfPos = self.pos1.copy().add(self.vel.getNormalized()/5)
+            incBallPos = projectile.pos.copy().add(projectile.vel.getNormalized()/5)
+            newSeperation = incSelfPos.subtract(incBallPos)
+            if newSeperation.length() <= seperation.length():
+                projectile.vel.reflect(seperation.getNormalized())
+        #End 2
+        seperation = self.pos2.copy().subtract(projectile.pos)
+        if (self.thickness) + projectile.radius >= seperation.length():
+            incSelfPos = self.pos2.copy().add(self.vel.getNormalized()/5)
+            incBallPos = projectile.pos.copy().add(projectile.vel.getNormalized()/5)
+            newSeperation = incSelfPos.subtract(incBallPos)
+            if newSeperation.length() <= seperation.length():
+                projectile.vel.reflect(seperation.getNormalized())
 
-            #Reflect
-            normal = self.pos1.copy().subtract(self.pos2).getNormal()
-            projectile.vel.reflect(normal)
+    def reflect(self,projectile):
+        if projectile.radius + self.thickness >= self.distanceTo(projectile):
+            if self.inBounds(projectile):
+                #Calculate how to free Object
+                direction:Vector = projectile.vel.copy().negate().normalize()
+                distance = ((projectile.radius+self.thickness)-self.distanceTo(projectile))*math.sin(self.line.angle(direction))
 
-            #Free Object
-            projectile.pos.add(direction.multiply(distance))#Going back by ammount overlapped
-            projectile.pos.add(projectile.vel.copy().normalize()*distance)#Adding lost distance after
-            return True;
-        return False;
+                #Reflect
+                normal = self.pos1.copy().subtract(self.pos2).getNormal().normalize()
+                projectile.vel.reflect(normal)
+
+                #Free Object
+                projectile.pos.add(direction.multiply(distance))#Going back by ammount overlapped
+                projectile.pos.add(projectile.vel.copy().normalize()*distance)#Adding lost distance after
+        else:
+            self.reflectEdge(projectile)
 
 #The projectile class is the object that is shot by the player e.g a bullet OR LASERS WHOOOOOOOOO
