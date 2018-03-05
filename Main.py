c63dbabfa30e8d57b8999ad0e652b50065a712ca
Sam Hollow <zeac114@live.rhul.ac.uk>
2018-03-05 11:53:30 +0000

    Changed how inventory wheel opens and closes

--- Main.py
+++ Main.py
@@ -13,33 +13,33 @@ from Classes.Inventory import Inventory
 
 import random
 
-CANVAS_HEIGHT=900
-CANVAS_WIDTH=1600
+CANVAS_HEIGHT = 900
+CANVAS_WIDTH = 1600
 
-#Defining Objects
+# Defining Objects
 character_image = simplegui._load_local_image('Resources/images/Deku_Link.png')
-character = Character(Vector(0,0),Vector(CANVAS_WIDTH/2,CANVAS_HEIGHT/2),character_image,0,(64,64))
+character = Character(Vector(0, 0), Vector(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2), character_image, 0, (64, 64))
 keyboard = Keyboard()
-inventory = Inventory(CANVAS_WIDTH,CANVAS_HEIGHT)
+inventory = Inventory(CANVAS_WIDTH, CANVAS_HEIGHT)
 map = Map()
 projectiles = []
 walls = map.walls
+
+
 # for i in range(100):
 #     projectiles.append( Projectile(Vector(random.randint(2,22),random.randint(2,22)),Vector(random.randint(0,CANVAS_WIDTH),random.randint(0,CANVAS_HEIGHT)),10,-1,True,0,"none") )
 #     walls.append( Wall(4,Vector(random.randint(0,CANVAS_WIDTH),random.randint(0,CANVAS_HEIGHT)),Vector(random.randint(0,CANVAS_WIDTH),random.randint(0,CANVAS_HEIGHT))) )
 
 # Handler to draw on canvas
 def draw(canvas):
-
-    #Interactions
+    # Interactions
     for wall in walls:
         for projectile in projectiles:
-            Interactions().bounceBallOffWall(projectile,wall)
+            Interactions().bounceBallOffWall(projectile, wall)
 
-
-    #Drawing and Updates
+    # Drawing and Updates
     map.draw(canvas)
-    canvas.draw_text("Testing", [50,112], 48, "Red")
+    canvas.draw_text("Testing", [50, 112], 48, "Red")
     character.draw(canvas)
     character.update(keyboard)
     character.pos.x %= CANVAS_WIDTH
@@ -51,27 +51,41 @@ def draw(canvas):
         proj.pos.x %= CANVAS_WIDTH
         proj.pos.y %= CANVAS_HEIGHT
 
-    #To see collision walls
+    # To see collision walls
     # for wall in walls:
     #     wall.draw(canvas)
 
-    inventory.draw(canvas,CANVAS_WIDTH,CANVAS_HEIGHT)
-    inventory.update(keyboard)
+    inventory.draw(canvas)
+    inventory.update(keyboard, character.pos.getP())
 
 
 def click(pos):
-    character.fire(Vector(pos[0],pos[1]),projectiles)
+    if inventory.isOpen:
+        inventory.select(pos)
+    else:
+        character.fire(Vector(pos[0], pos[1]), projectiles)
+
+
+def drag(pos):
+    if inventory.isOpen:
+        inventory.drag(pos)
+
+
 def keyDown(key):
     keyboard.keyDown(key)
+
+
 def keyUp(key):
     keyboard.keyUp(key)
+
+
 # Create a frame and assign callbacks to event handlers
 frame = simplegui.create_frame("Action Game", CANVAS_WIDTH, CANVAS_HEIGHT)
 frame.set_mouseclick_handler(click)
+frame.set_mousedrag_handler(drag)
 frame.set_keydown_handler(keyDown)
 frame.set_keyup_handler(keyUp)
 frame.set_draw_handler(draw)
-
 
 # Start the frame animation
 frame.start()
