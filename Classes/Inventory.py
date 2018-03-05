7157cad343a305568a85043e6fdcce153edb0e92
Sam Hollow <zeac114@live.rhul.ac.uk>
2018-03-05 15:45:50 +0000

    Added inventory slot selection

--- Classes/Inventory.py
+++ Classes/Inventory.py
@@ -12,9 +12,11 @@ class Inventory:
         self.abilityCount = 7  # Temporary ability count for testing purposes
         self.released = True  # Stores if the inventory button has been released while the inventory is open
         self.isOpen = False  # Determines if the inventory should be open or closed
+        self.isDragged = False
         self.radius = min(CANVAS_WIDTH, CANVAS_HEIGHT) // 9  # Radius of the inventory wheel
         self.thickness = min(CANVAS_WIDTH, CANVAS_HEIGHT) // 12  # Thickness of the inventory wheel
         self.pos = []  # Position of the inventory wheel
+        self.angle = 0  # Angle of the line between the inventory wheel and cursor
 
     def update(self, keyboard, pos):  # Update method to be called each game loop
         self.pos = pos
@@ -34,16 +36,28 @@ class Inventory:
                                self.radius, self.thickness, '#666666', )
 
             for i in range(self.abilityCount):
+                radius = self.thickness // 2.2  # Radius of inventory wheel slots
+                if ((i - 0.5) / self.abilityCount * 2 * math.pi) <= self.angle < (
+                        (i + 0.5) / self.abilityCount * 2 * math.pi) and self.isDragged:
+                    radius = self.thickness // 1.8
+                #  Split for clarity
+                elif ((i - 0.5 + self.abilityCount) / self.abilityCount * 2 * math.pi) <= self.angle and self.isDragged:
+                    radius = self.thickness // 1.8
+
                 canvas.draw_circle(
                     (self.pos[0] + self.radius * -math.sin(i / self.abilityCount * 2 * math.pi),
                      self.pos[1] + self.radius * -math.cos(i / self.abilityCount * 2 * math.pi)),
-                    self.thickness // 2.2, 2, '#555555', '#777777')
+                    radius, 2, '#555555', '#777777')
 
     def drag(self, pos):  # Highlights the selected powerup or ability
-        pass
+        self.isDragged = True
+        self.angle = math.atan2(self.pos[0] - pos[0], self.pos[1] - pos[1])
+        if self.angle < 0:
+            self.angle += 2 * math.pi
 
     def select(self, pos):  # Select either a powerup or ability in the inventory wheel
         self.isOpen = False
+        self.isDragged = False
 
     def enableAbility(self):  # Weapon being used
         pass
