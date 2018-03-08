939a6ebfa7008aaabcdc1c51de99ef043b6dafb9
= <Keyurschool8@gmail.com>
2018-03-08 19:43:54 +0000

    Enemy sprites,Map update,Wall update

--- Classes/Enemy/Enemy2.py
+++ Classes/Enemy/Enemy2.py
@@ -1,20 +1,19 @@
 from Classes.Enemy.Line import Line
 from Classes.Vector import Vector
 from Classes.Abilities.Cannon import Cannon
-from Classes.Objects.Projectile import Projectile
+from Classes.Enemy.EnemySuper import EnemySuper
+from Classes.Enemy.Enemy2IMG import Enemy2IMG
 
-class Enemy2:
-    def __init__(self,pos:Vector,color,length,Line,type):
+class Enemy2(EnemySuper):
+    def __init__(self,pos:Vector,color,type,bat):
         self.pos = pos
         self.vel = Vector()
         self.radius = 30
-        self.length = length
+        self.length = 300
         self.normalLine = Vector(0,-self.length)
         self.lineLeftGen = Vector(-self.radius*2 ,-self.length)
         self.lineRightGen = Vector(+self.radius*2, -self.length)
-        self.line = Line
         self.direction = Vector()
-        self.looking = False
         self.rotate = False
         self.rotation = 5
         self.losColour = 'rgba(255,255,0,0.6)'
@@ -23,28 +22,15 @@ class Enemy2:
         self.soundRange = 100
         self.stealthRange = 150
         self.color = color
-
-        self.speed = 0.3
+        self.speed = 0.6
         self.health = 100
         self.ability = Cannon()
-
+        self.enemyIMG = Enemy2IMG(self.pos, bat,330,232,9,4,[0,2],100,100,0)
         self.updateLOS()
 
-    def fire(self,pos:Vector,projectiles:list):
-        self.ability.fire(pos,projectiles,self.pos,"enemy")
-
-
-    def inLOS(self,player):
-        leftVision:Vector = self.pos.copy().subtract(self.leftgen)
-        rightVision:Vector = self.pos.copy().subtract(self.rightgen)
-        playerVector = self.pos-player.pos
-
-        if self.length >= playerVector.length():
-            if playerVector.angleToX() >= leftVision.angleToX() and playerVector.angleToX() <= rightVision.angleToX():
-                self.found = True
-                self.normalBoundary.color = "red"
-        elif self.length*2 < playerVector.length():
-            self.found = False
+    def spriteUpdate(self,character,enemy):
+        self.enemyIMG.updateDirection(character,enemy)
+        self.enemyIMG.update()
 
     def updateLOS(self):
         self.normalGen = Vector((self.pos.x + self.normalLine.x), (self.pos.y + self.normalLine.y))
@@ -56,9 +42,6 @@ class Enemy2:
         self.rightgen = Vector((self.pos.x + self.lineRightGen.x), (self.pos.y + self.lineRightGen.y))
         self.rightBoundary = Line(self.pos, self.rightgen, "white")
 
-    def updateSoundDistance(self,player):
-        if self.soundRange >= (self.pos-player.pos).length():
-            self.found = True
 
     def update(self,zoom,player):
         self.updateLOS()
@@ -70,70 +53,19 @@ class Enemy2:
         self.pos.add(self.vel*zoom)
 
     def drawLos(self, canvas, offset):
-        canvas.draw_polygon([((self.leftBoundary.pA.x + offset.x), (self.leftBoundary.pA.y + offset.y)),
+        canvas.draw_polygon([((self.leftBoundary.pA.x + offset.x), ((self.leftBoundary.pA.y-25) + offset.y)),
                              ((self.leftBoundary.pB.x + offset.x), (self.leftBoundary.pB.y + offset.y)),
                              ((self.rightBoundary.pB.x + offset.x), (self.rightBoundary.pB.y + offset.y)), ], 0, "white",
                             self.losColour)
 
-    def draw(self,canvas,offset):
+    def draw(self,canvas,offset,bat,enemy,player):
         self.drawLos(canvas,offset)
-        canvas.draw_circle((self.pos+offset).getP(), self.radius, 1, self.color, self.color)
+        self.enemyIMG.draw(canvas,offset)
+        # canvas.draw_circle((self.pos+offset).getP(), self.radius, 1, self.color, self.color)
 
-    def healthBar(self,canvas):
-        line1 = Line(Vector(self.pos.x,self.pos.y-100),Vector(self.pos.x+self.health,self.pos.y),"Red")
-        line2 = Line(Vector(self.pos.x,self.pos.y-100),Vector(self.pos.x+100,self.pos.y),"white")
-        line1.draw(canvas)
-        line2.draw(canvas)
-
-    def stealthDistance(self,player):
-        if self.stealthRange >= (self.pos-player.pos).length() and (self.pos-player.pos).length() > self.soundRange:
-            return True
-        return False
-
-    def alertDistance(self):
-        pass
-
-    def stop(self,player):
-        if (self.radius+player.radius)+20 >= (self.pos-player.pos).length():
-            self.direction = Vector(0,0)
-
-    def attack(self,player):
+    def attack(self,player,health):
         if (self.radius + player.radius) + 20 >= (self.pos-player.pos).length():
             self.direction = Vector(0, 0)
+            health.damageTaken()
             player.vel.add(player.vel.getNormalized()*20)
             player.health -= 10
-
-    def follow(self,player):
-        self.losColour = 'rgba(255,0,0,0.6)'
-        playertest = player.pos.copy().subtract(self.pos)
-        self.direction = playertest
-
-    def search(self,player):
-        playerVector = self.pos-player.pos
-        vectorOfLeft = self.leftBoundary.pA - self.leftBoundary.pB
-        Vectorleft = self.pos.copy().subtract(vectorOfLeft)
-        Left = self.pos.copy().subtract(Vectorleft)
-
-        angleLess = Left.getNormal().angle(playerVector.getNormal())
-        vectorToNormal = (self.normalBoundary.pA - self.normalBoundary.pB)
-        VectorNormal = self.pos.copy().subtract(vectorToNormal)
-        normal = self.pos.copy().subtract(VectorNormal)
-
-        tester = playerVector.getNormal().angle(normal.getNormal())
-
-        if self.found:
-            if round(angleLess,2) > round(tester,2):
-                self.lineLeftGen.rotate(self.rotation)
-                self.normalLine.rotate(self.rotation)
-                self.lineRightGen.rotate(self.rotation)
-            elif round(angleLess,2) < round(tester,2):
-                self.lineLeftGen.rotate(-self.rotation)
-                self.normalLine.rotate(-self.rotation)
-                self.lineRightGen.rotate(-self.rotation)
-
-    def distanceToEnemy(self,enemy1:Vector,enemy2:Vector):
-        distance = enemy1.pos.copy().subtract(enemy2.pos)
-        if distance.length() < 55:
-            print("Test")
-
-    #Add a mirror which will reflect the line of sight
--- Main.py
+++ Main.py
@@ -2,9 +2,8 @@ try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-from Classes.Objects.Projectile import Projectile
+
 from Classes.Interactions import Interactions
-from Classes.Objects.Wall import Wall
 from Classes.Vector import Vector
 from Classes.MainCharacter import Character
 from Classes.MainCharacter import Keyboard
@@ -12,18 +11,21 @@ from Classes.Maps.Map import Map
 from Classes.View import View
 from Classes.Enemy.Enemy import Enemy
 from Classes.Enemy.Enemy2 import Enemy2
-from Classes.Enemy.Line import Line
 from Classes.Inventory import Inventory
-
-
-import random
+from Classes.Enemy.Line import Line
+from Classes.healthIMG import HealthIMG
+from Classes.Health import Health
 
 CANVAS_HEIGHT=900
 CANVAS_WIDTH=1600
 offset = Vector(0,0)
-
-# Defining Objects
+i = 0
+#Defining Objects
 character_image = simplegui._load_local_image('Resources/images/Deku_Link.png')
+heart1 = simplegui._load_local_image('Resources/images/Health.png')
+Bat = simplegui._load_local_image('Resources/images/Bat.png')
+FireEnemy = simplegui._load_local_image('Resources/images/FireEnemy.png')
+
 frame = simplegui.create_frame("Action Game", CANVAS_WIDTH, CANVAS_HEIGHT)
 keyboard = Keyboard()
 map = Map(frame,CANVAS_WIDTH,CANVAS_HEIGHT)
@@ -32,21 +34,36 @@ offset = -map.startPos+(Vector(CANVAS_WIDTH, CANVAS_HEIGHT)/2)
 inventory = Inventory(CANVAS_WIDTH, CANVAS_HEIGHT)
 projectiles = []
 walls = map.walls
-enemies = [Enemy(Vector(900,1500),"Red",300,Line,"sniper"),Enemy2(Vector(1200,1000),"Blue",300,Line,"malee")]
+enemies = [Enemy(Vector(900,1600),"Red","Sniper",FireEnemy),Enemy2(Vector(1200,1000),"Blue","Malee",Bat)]
+heart1OB = HealthIMG(Vector(50,50),heart1)
+heart2OB = HealthIMG(Vector(100,50),heart1)
+heart3OB = HealthIMG(Vector(150,50),heart1)
+healthOB = [heart1OB,heart2OB,heart3OB]
+health = Health(heart1OB,heart2OB,heart3OB)
+incrementalTimer = 0
 
 # Handler to draw on canvas
 def attack():
     for enemy in enemies:
         if enemy.found:
             enemy.follow(character)
-            if enemy.type == "sniper":
+            if enemy.type == "Sniper":
                 enemy.fire(character.pos, projectiles)
-            elif enemy.type == "malee":
-                enemy.attack(character)
+            elif enemy.type == "Malee":
+                enemy.attack(character,health)
         if not enemy.found:
             enemy.losColour = 'rgba(255,255,0,0.6)'
 
 def draw(canvas):
+    global incrementalTimer
+    i = 0
+    while i < enemies.__len__()-1:
+        k = enemies.__len__()-1
+        while k > i:
+            enemies[i].alertDistance(enemies[k])
+            k -= 1
+        i += 1
+
     #Interactions
     for wall in walls:
         for projectile in projectiles:
@@ -58,19 +75,23 @@ def draw(canvas):
     character.update(keyboard,map.zoom)
 
     for enemy in enemies:
-        enemy.draw(canvas,offset)
+        enemy.draw(canvas,offset,Bat,enemy,character)
         enemy.update(map.zoom,character)
         if enemy.found:
+            if incrementalTimer%5 == 0:
+                enemy.spriteUpdate(character, enemy)
+            incrementalTimer += 1
             enemy.search(character)
             enemy.stop(character)
+        elif not enemy.found:
+            enemy.direction = Vector(0,0)
 
     #Moving Screen
     View().moveScreen(offset,character.pos,CANVAS_WIDTH,CANVAS_HEIGHT)
-
     for proj in projectiles:
         proj.draw(canvas,offset)
         proj.update(projectiles,map.zoom)
-        Interactions().ballHitPlayer(proj,character,projectiles)
+        Interactions().ballHitPlayer(proj,character,projectiles,health)
 
         for enemy in enemies:
             Interactions().ballHitEnemy(proj,projectiles,enemy,enemies)
@@ -83,8 +104,12 @@ def draw(canvas):
     inventory.update(keyboard, (character.pos+offset).getP())
 
     #Draw HUD
-    canvas.draw_text("Testing", [50,112], 48, "white")
-    canvas.draw_text("Health: "+str(character.health), [50, 200], 48, "Red")
+    i = healthOB.__len__()-1
+    while i >= 0:
+        healthOB[i].draw(canvas, offset)
+        i -= 1
+    # canvas.draw_text("Testing", [50,112], 48, "white")
+    # canvas.draw_text("Health: "+str(character.health), [50, 200], 48, "Red")
 
 def click(pos):
     if inventory.isOpen:
@@ -110,7 +135,7 @@ frame.set_mousedrag_handler(drag)
 frame.set_keydown_handler(keyDown)
 frame.set_keyup_handler(keyUp)
 frame.set_draw_handler(draw)
-timer = simplegui.create_timer(1000, attack)
+timer = simplegui.create_timer(500, attack)
 timer.start()
 # Start the frame animation
 frame.start()
