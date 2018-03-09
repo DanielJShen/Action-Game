21eedd1a17ed6b8acf8162b0b03a886b7ecb8736
= <Keyurschool8@gmail.com>
2018-03-09 01:23:49 +0000

    orientation fix,overlap fix

--- Classes/Enemy/Bat.py
+++ Classes/Enemy/Bat.py
@@ -5,21 +5,18 @@ from Classes.Enemy.EnemySuper import EnemySuper
 from Classes.Enemy.Enemy2IMG import Enemy2IMG
 
 class Bat(EnemySuper):
-    def __init__(self,pos:Vector,color,type,image,rotation=0):
-
+    def __init__(self,pos:Vector,color,type,image,FrameIndex,rotation=0):
         self.defineVariables(pos,color,type,image)
-
         self.soundRange = 100
         self.stealthRange = 150
         self.speed = 0.6
         self.health = 100
         self.ability = Cannon()
-        self.enemyIMG = Enemy2IMG(self.pos, image,330,232,9,4,[0,2],100,100,0)
+        self.enemyIMG = Enemy2IMG(self.pos, image,330,232,9,4,FrameIndex,100,100,0)
 
         self.lineLeftGen.rotate(rotation)
         self.normalLine.rotate(rotation)
         self.lineRightGen.rotate(rotation)
-
         self.updateLOS()
 
     def attack(self,player,health):
@@ -28,8 +25,3 @@ class Bat(EnemySuper):
             health.damageTaken()
             self.vel.add(self.vel.getNormalized().negate()*12)
             player.health -= 10
-
-    def draw(self,canvas,offset,enemy,player):
-        if not self.found:
-            self.drawLos(canvas, offset)
-        self.enemyIMG.draw(canvas, offset)
