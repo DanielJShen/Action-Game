324a84a1a6acc2dd7aa656d0c9b5b21b281bde64
Sam Hollow <zeac114@live.rhul.ac.uk>
2018-03-10 22:35:00 +0000

    Added Animation Class

---
+++ Classes/Animation.py
@@ -0,0 +1,10 @@
+from Classes.Spritesheet import Spritesheet
+
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+
+class Animation(Spritesheet):
+    pass
