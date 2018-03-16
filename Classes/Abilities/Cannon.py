c13e51386141467068d3eff1ca1a2136240188a0
Daniel <6959648+DanielJShen@users.noreply.github.com>
2018-03-16 17:30:19 +0000

    Small fix

--- Classes/Abilities/Cannon.py
+++ Classes/Abilities/Cannon.py
@@ -7,14 +7,15 @@ class Cannon():
     def __init__(self):
         self.sprite = object
         self.radius = 10
-        self.colour = "#7a14ff"
+        self.projectileImage = simplegui._load_local_image("Resources/images/Laser.png")
         self.time = 1.5
         self.bounce = True
         self.baseDamage = 10
         self.image = simplegui._load_local_image("Resources/images/Cannon.png")
 
 
-    def fire(self,toPos,projectiles,lasers,fromPos,owner,image):
+    def fire(self,toPos,projectiles,lasers,fromPos,owner,image=None):
+        if image==None: image=self.projectileImage
         vel = toPos.copy().subtract(fromPos).getNormalized()*30
         projectiles.append( Projectile(vel,fromPos.copy(),self.radius,self.time,self.bounce,self.baseDamage,owner,image) )
 
